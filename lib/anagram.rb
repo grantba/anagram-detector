b3f60e6a1015e94b8dd67059fc3b0fb621fae976
Blaire <grant.blaire@gmail.com>
2020-11-24 18:27:12 -0600

    commit tree 7a77e85bee14e4c0ce85d31846cc930874c34f94

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(some_words)
+        some_words.select do |check_word|
+            check_word.split("").sort == @word.split("").sort 
+        end
+    end
+
+end
+
+detector = Anagram.new('ba')
+ba = detector.match(['ab', 'abc', 'bac'])
